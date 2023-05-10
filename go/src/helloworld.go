b7e1ffdf0206cdd82a2ac5f242521b9458527fde
MASAFFFF <16806394+masaffff@users.noreply.github.com>
2023-05-10 11:19:53 +0900

    Update helloworld.go Showcase

--- go/src/helloworld.go
+++ go/src/helloworld.go
@@ -30,7 +30,7 @@ func main() {
 	}
 	log.Println("Starting up " + version)
 	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<body style="background-color:blue;"><h1>Hello, World " + version + "!</h1><body>")
+		fmt.Fprintf(w, "Welcome to IBM Technology Showcase: " + version + "!</h1><body>")
 	})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
